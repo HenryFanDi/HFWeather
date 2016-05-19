26088c5ae80832c77f3ae122f15980702a2ab80c
Henry Fan <henry.fan@centraltrillion.com>
2016-05-19 18:40:38 +0800

    feat: add mainViewController 1. some of project default settings. 2. remove main storyboard file.

--- Weather/MainViewController.swift
+++ Weather/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Weather
 //
 //  Created by HenryFan on 19/5/2016.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+  
+  // MARK: Lifecycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
+  
 }
-
